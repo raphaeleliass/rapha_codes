1b1bdd845bf183899ff0fd3929ca48ee00e05d64
Raphael Elias <raphaeleliass@outlook.com>
2025-09-26 19:14:25 -0300

    feat(server): added database migrations

---
+++ apps/server/src/db/migrations/0004_blue_nemesis.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "post" DROP CONSTRAINT "post_user_id_user_id_fk";
+--> statement-breakpoint
+ALTER TABLE "post" ADD CONSTRAINT "post_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;
