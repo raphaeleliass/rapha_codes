8309f52561aa5d66514977272f462c72ff7828b3
Raphael Elias <raphaeleliass@outlook.com>
2025-09-27 10:24:32 -0300

    feat(server): added publicRoutes on appRouter

--- apps/server/src/routers/index.ts
+++ apps/server/src/routers/index.ts
@@ -1,6 +1,8 @@
 import { postRoutes } from "./post.routes";
+import { publicRoutes } from "./public.routes";
 
 export const appRouter = {
 	postRoutes,
+	publicRoutes,
 };
 export type AppRouter = typeof appRouter;
