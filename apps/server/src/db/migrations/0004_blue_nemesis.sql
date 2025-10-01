daf04aa51d14072880c55c3047ef94339a4cbedc
Raphael Elias <raphaeleliass@outlook.com>
2025-10-01 20:51:09 -0300

    refactor(server): removed unused migrations files

--- apps/server/src/db/migrations/0004_blue_nemesis.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "post" DROP CONSTRAINT "post_user_id_user_id_fk";
---> statement-breakpoint
-ALTER TABLE "post" ADD CONSTRAINT "post_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;
