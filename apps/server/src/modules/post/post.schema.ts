f92d14baa104c2a2b40145d592dc0054ff939fd2
Raphael Elias <raphaeleliass@outlook.com>
2025-10-01 20:57:10 -0300

    fix(server): correct capitalization in post schema validation messages

--- apps/server/src/modules/post/post.schema.ts
+++ apps/server/src/modules/post/post.schema.ts
@@ -3,19 +3,22 @@ import z from "zod";
 export const postSchema = z.object({
 	id: z
 		.string("Field id must be a string")
-		.nonempty("field id cannot be empty"),
+		.nonempty("Field id cannot be empty"),
+	authorImg: z
+		.string("Field author_img must be a string")
+		.nonempty("Field author_img cannot be empty"),
 	author: z
 		.string("Field author must be a string")
-		.nonempty("field author cannot be empty"),
+		.nonempty("Field author cannot be empty"),
 	title: z
 		.string("Field title must be a string")
-		.nonempty("field title cannot be empty"),
+		.nonempty("Field title cannot be empty"),
 	content: z
 		.string("Field content must be a string")
-		.nonempty("field content cannot be empty"),
+		.nonempty("Field content cannot be empty"),
 	draft: z
-		.boolean("Field draft must be a boolean")
-		.nonoptional("field draft cannot be null"),
+		.boolean("Field draFt must be a boolean")
+		.nonoptional("Field draFt cannot be null"),
 });
 
 export const createPostSchema = postSchema.omit({ id: true }).strict();
