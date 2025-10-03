eecd49ea17f5ebc581101ea9e72ffe11dcabb3fc
Raphael Elias <raphaeleliass@outlook.com>
2025-10-02 21:22:09 -0300

    feat(web): add text editor

--- apps/web/src/components/editor.tsx
+++ apps/web/src/components/editor.tsx
@@ -1,7 +1,24 @@
 "use client";
+import CodeBlockLowLight from "@tiptap/extension-code-block-lowlight";
 import { EditorContent, useEditor } from "@tiptap/react";
 import { StarterKit } from "@tiptap/starter-kit";
+import css from "highlight.js/lib/languages/css";
+import js from "highlight.js/lib/languages/javascript";
+import ts from "highlight.js/lib/languages/typescript";
+import html from "highlight.js/lib/languages/xml";
+import { all, createLowlight } from "lowlight";
 import { useEffect } from "react";
+
+import "highlight.js/styles/tokyo-night-dark.css";
+import BubbleMenu from "./ui/bubble-menu";
+import { Button } from "./ui/button";
+
+const lowlight = createLowlight(all);
+
+lowlight.register("html", html);
+lowlight.register("css", css);
+lowlight.register("js", js);
+lowlight.register("ts", ts);
 
 interface EditorProps {
 	content: string;
@@ -9,18 +26,38 @@ interface EditorProps {
 }
 
 export default function Editor({ content, editable = false }: EditorProps) {
-	const tiptap = useEditor({
-		extensions: [StarterKit],
+	const editor = useEditor({
+		extensions: [
+			StarterKit,
+			CodeBlockLowLight.configure({
+				lowlight,
+			}),
+		],
+		editorProps: {
+			attributes: {
+				class: "outline-none border-t pt-8 prose prose-theme w-full max-w-none",
+			},
+		},
 		content,
 		editable,
 		immediatelyRender: false,
 	});
 
 	useEffect(() => {
-		if (tiptap) {
-			tiptap.setEditable(editable);
+		if (editor) {
+			editor.setEditable(editable);
 		}
-	}, [editable, tiptap]);
+	}, [editable, editor]);
 
-	return <EditorContent editor={tiptap} />;
+	return (
+		<>
+			<EditorContent editor={editor} />
+			<BubbleMenu editor={editor} />
+
+			<Button onClick={() => editor?.commands.setContent("teste")}>
+				trocar o conteudo do editor para "teste"
+			</Button>
+			<Button onClick={() => editor?.commands.undo()}>desfazer</Button>
+		</>
+	);
 }
