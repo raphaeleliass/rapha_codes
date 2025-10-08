4b689622379b46ef4faa37ff13a21186ca0791ac
Raphael Elias <raphaeleliass@outlook.com>
2025-10-08 16:46:37 -0300

    feat(web): implement cookie retrieval and user session management in Providers component

--- apps/web/src/components/providers.tsx
+++ apps/web/src/components/providers.tsx
@@ -1,9 +1,40 @@
 "use client";
 
+import { useEffect } from "react";
+import { Toaster } from "sonner";
+import fetchAllPosts from "@/actions/fetchAllPosts";
+import { authClient } from "@/lib/auth-client";
+import { usePostStore } from "@/store/usePostStore";
+import { useUserStore } from "@/store/useUserStore";
 import Navbar from "./layout/Navbar";
 import { ThemeProvider } from "./theme-provider";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
+	const { setUser, user } = useUserStore();
+	const { setPosts } = usePostStore();
+	// biome-ignore lint: not necessary include user at array of dependencies
+	useEffect(() => {
+		if (user) return;
+
+		(async () => {
+			const cookie = await fetch("/api/get-cookie");
+
+			if (!cookie.ok) return;
+
+			const { data } = await authClient.getSession();
+
+			if (data?.user) setUser(data.user);
+		})();
+	}, [setUser]);
+
+	useEffect(() => {
+		(async () => {
+			const postsData = await fetchAllPosts();
+
+			setPosts(postsData);
+		})();
+	}, []);
+
 	return (
 		<ThemeProvider
 			attribute="class"
@@ -12,7 +43,9 @@ export default function Providers({ children }: { children: React.ReactNode }) {
 			disableTransitionOnChange
 		>
 			<Navbar />
+
 			{children}
+			<Toaster richColors position="top-center" />
 		</ThemeProvider>
 	);
 }
