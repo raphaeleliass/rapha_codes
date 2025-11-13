137b95bc88932c1eac408ad96b35f24c0f311934
Raphael Elias <raphaeleliass@outlook.com>
2025-11-13 15:12:38 -0300

    fix(auth): add nextCookies plugin to betterAuth configuration

--- apps/server/src/lib/auth.ts
+++ apps/server/src/lib/auth.ts
@@ -1,5 +1,6 @@
 import { type BetterAuthOptions, betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
 import { username } from "better-auth/plugins";
 import { db } from "../db";
 import * as schema from "../db/schema/auth";
@@ -14,7 +15,7 @@ export const auth = betterAuth<BetterAuthOptions>({
 	emailAndPassword: {
 		enabled: true,
 	},
-	plugins: [username()],
+	plugins: [username(), nextCookies()],
 	advanced: {
 		defaultCookieAttributes: {
 			sameSite: "none",
