dc489644b327ebfac57f4dcc6de2bacf03dd48fc
Raphael Elias <raphaeleliass@outlook.com>
2025-11-13 10:27:06 -0300

    update: remove cache component from components

--- apps/web/src/app/(routes)/dashboard/page.tsx
+++ apps/web/src/app/(routes)/dashboard/page.tsx
@@ -1,13 +1,8 @@
-import { cacheLife } from "next/cache";
 import { Suspense } from "react";
 import Dashboard from "@/components/layout/dashboard/Dashboard";
 import LoadingPage from "@/components/ui/loadingPage";
 
 export default async function Page() {
-	"use cache";
-
-	cacheLife("minutes");
-
 	return (
 		<Suspense fallback={<LoadingPage />}>
 			<Dashboard />
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,14 +1,9 @@
-import { cacheLife } from "next/cache";
 import { Suspense } from "react";
 import Footer from "@/components/layout/Footer";
 import Posts from "@/components/layout/Posts";
 import LoadingPage from "@/components/ui/loadingPage";
 
 export default async function Page() {
-	"use cache";
-
-	cacheLife("minutes");
-
 	return (
 		<main>
 			<Suspense fallback={<LoadingPage />}>
