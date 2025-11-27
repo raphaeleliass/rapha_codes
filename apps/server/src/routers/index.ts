1664ad0aecad17fdcd36bdcf4535cbec41cee1a5
Raphael Elias <raphaeleliass@outlook.com>
2025-11-27 01:25:00 -0300

    refactor(router): remove appRouter and associated route imports

--- apps/server/src/routers/index.ts
+++
@@ -1,8 +0,0 @@
-import { postRoutes } from "./post.routes";
-import { publicRoutes } from "./public.routes";
-
-export const appRouter = {
-	postRoutes,
-	publicRoutes,
-};
-export type AppRouter = typeof appRouter;
